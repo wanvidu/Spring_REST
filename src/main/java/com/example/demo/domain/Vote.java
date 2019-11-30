0e7eca7f73abfd20d8431d9b0737ba84c51c670c
wanvidu <wanvidu@gmail.com>
2019-11-30 06:13:10 +0000

    Domain

---
+++ src/main/java/com/example/demo/domain/Vote.java
@@ -0,0 +1,61 @@
+package com.example.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "VOTE_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "OPTION_ID")
+    private Option option;
+
+    public Vote(Long id, Option option) {
+        this.id = id;
+        this.option = option;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Option getOption() {
+        return this.option;
+    }
+
+    public void setOption(Option option) {
+        this.option = option;
+    }
+
+    public Vote id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Vote option(Option option) {
+        this.option = option;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", option='" + getOption() + "'" +
+            "}";
+    }
+
+}
